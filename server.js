d1f6f7ee73b923856e9866aa29c270c0bae6b118
ISingh86 <imindersingh@gmail.com>
2020-02-14 00:11:33 +0000

    Adding ejs to post data via form and display all commands

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ const express = require('express'),
   app = express(),
   port = process.env.PORT || 8080,
   mongoose = require('mongoose'),
-  Task = require('./api/models/model'),
+  model = require('./api/models/model'),
   bodyParser = require('body-parser');
   
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mongo:27017/db', {useNewUrlParser : true, useUnifiedTopology : true}); 
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.get('/', (req, res) =>{
+  model.find(function(err, command) {
+    if (err)
+      res.send(err);
+    res.render("index", { command : command});   
+  });
+})
 
 const routes = require('./api/routes/routes');
 routes(app);
